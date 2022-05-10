0fb7d2f7f7e6363bc010a0c533d7e3b7c68a71b5
LeraDolgova109 <Dol0914@yandex.ru>
2022-05-10 18:54:26 +0700

    вариант реализации с ресайклером
    добавлен ресайкл вью, некорректно отображаются темы

--- app/src/main/java/com/example/pollernas/MainActivity.kt
+++ app/src/main/java/com/example/pollernas/MainActivity.kt
@@ -1,11 +1,111 @@
 package com.example.pollernas
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Spinner
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.ernestoyaquello.dragdropswiperecyclerview.DragDropSwipeRecyclerView
+import com.ernestoyaquello.dragdropswiperecyclerview.listener.OnItemSwipeListener
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var dragDropAdapter: DragDropAdapter
+    private var itemsList = mutableListOf<String>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        createFakeItems()
+        setUpRecycler()
+    }
+
+    private fun createFakeItems() {
+        itemsList.add("Ввод переменных")
+        itemsList.add("Присваивание")
+    }
+
+    private fun setUpRecycler() {
+        dragDropAdapter =
+            DragDropAdapter(itemsList)
+        val mList: DragDropSwipeRecyclerView = findViewById(R.id.list)
+        mList.layoutManager = LinearLayoutManager(this)
+        mList.adapter = dragDropAdapter
+
+        mList.orientation =
+            DragDropSwipeRecyclerView.ListOrientation.VERTICAL_LIST_WITH_VERTICAL_DRAGGING
+        mList.reduceItemAlphaOnSwiping = true
+
+        //mList.disableSwipeDirection(DragDropSwipeRecyclerView.ListOrientation.DirectionFlag.RIGHT)
+
+        val onItemSwipeListener = object : OnItemSwipeListener<String> {
+            override fun onItemSwiped(
+                position: Int,
+                direction: OnItemSwipeListener.SwipeDirection,
+                item: String
+            ): Boolean {
+                Log.d("Main", "Position = $position, Direction = $direction, Item = $item")
+
+                when (direction) {
+                    //Delete Item
+                    OnItemSwipeListener.SwipeDirection.RIGHT_TO_LEFT -> {
+                        Toast.makeText(
+                            applicationContext,
+                            "Блок $item удален",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                        //todo: add deleted code
+                    }
+                    //Archive Item
+                    OnItemSwipeListener.SwipeDirection.LEFT_TO_RIGHT -> {
+                        Toast.makeText(
+                            applicationContext,
+                            "Блок $item архивирован",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                        //todo: add archived code here
+                    }
+                    else -> return false
+                }
+                return false
+            }
+        }
+        mList.swipeListener = onItemSwipeListener
+
+        // button
+        fabAddItem()
+    }
+
+    private fun fabAddItem() {
+        fab_add.setOnClickListener {
+            Log.d("Main", "Button pressed")
+            val builder = AlertDialog.Builder(this)
+            val inflater = layoutInflater
+            val dialogLayout = inflater.inflate(R.layout.edit_text_layout, null)
+            val spinner = dialogLayout.findViewById<Spinner>(R.id.spinner)
+
+            with(builder) {
+                setTitle("Выбери нужный блок")
+                setPositiveButton("OK") { dialog, which ->
+                    dragDropAdapter.updateItem(spinner.selectedItem.toString())
+
+
+                    Toast.makeText(
+                        applicationContext,
+                        "Блок успешно добавлен!",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+                setNegativeButton("Отмена") { dialog, which ->
+                    Log.d("Main", "Negative button clicked")
+                }
+                setView(dialogLayout)
+                show()
+            }
+        }
     }
 }
